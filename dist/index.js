8911f758058e2b15e3587b2ff93651dda48599c4
Ryota Nomura <ryota-nomura@c-fo.com>
2020-08-21 21:53:42 +0900

    Add action to attach pr to card

--- dist/index.js
+++ dist/index.js
@@ -18,19 +18,95 @@ var __importStar = (this && this.__importStar) || function (mod) {
     __setModuleDefault(result, mod);
     return result;
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const core = __importStar(require("@actions/core"));
 const github = __importStar(require("@actions/github"));
+const node_fetch_1 = __importDefault(require("node-fetch"));
+const log4js_1 = __importDefault(require("log4js"));
+const logger = log4js_1.default.getLogger();
+logger.level = "all";
+const resolveTrelloUrlFrom = (text) => {
+    logger.info(text);
+    if (text) {
+        return text.match(/https:\/\/trello\.com\/c\/.*/g) || [];
+    }
+    else {
+        return [];
+    }
+};
+const createTrelloContext = () => {
+    const apiKey = process.env["TRELLO_API_KEY"];
+    const apiToken = process.env["TRELLO_API_TOKEN"];
+    const boardId = core.getInput("board-id");
+    if (!apiKey) {
+        throw new Error("TRELLO_API_KEY not defined");
+    }
+    if (!apiToken) {
+        throw new Error("TRELLO_API_TOKEN not defined");
+    }
+    if (!boardId) {
+        throw new Error("board-id not defined");
+    }
+    return {
+        apiKey,
+        apiToken,
+        boardId,
+    };
+};
+const attachPrToCard = (trelloContext, trelloUrls, prUrl) => {
+    if (trelloUrls.length === 0) {
+        logger.info("target card not fount");
+        return;
+    }
+    logger.info(`target cards: ${trelloUrls}`);
+    if (!prUrl) {
+        throw new Error("pull-request url is not defined");
+    }
+    trelloUrls.forEach((url) => {
+        const cardId = (url.match(/https:\/\/trello\.com\/c\/(.*)/) ||
+            new Array(2))[1];
+        logger.info(`Attaching github url to card: ${cardId}`);
+        node_fetch_1.default(`https://trello.com/1/cards/${cardId}/attachments`, {
+            method: "POST",
+            body: new URLSearchParams({
+                key: trelloContext.apiKey,
+                token: trelloContext.apiToken,
+                url: prUrl,
+            }),
+        })
+            .then((r) => {
+            r.text().then((text) => logger.info(`response: ${text}`));
+            if (!r.ok) {
+                throw new Error(`error occurred while updating ${url}`);
+            }
+            logger.info(`successfully updated ${url}`);
+        })
+            .catch((error) => {
+            logger.info(`error: ${error}`);
+            throw error;
+        });
+    });
+};
+const moveCard = (trelloContext, trelloUrls, destListId) => {
+    logger.info("hoge");
+};
 try {
     const targetActionName = core.getInput("target-action-name");
-    console.log(targetActionName);
+    logger.info(`target action is ${targetActionName}`);
     if (github.context.payload.pull_request) {
-        const title = github.context.payload.pull_request.title;
-        const description = github.context.payload.pull_request.body;
-        const author = github.context.payload.pull_request.user.login;
-        console.log(title);
-        console.log(author);
-        console.log(description);
+        switch (targetActionName) {
+            case "attach-pr-to-card":
+                attachPrToCard(createTrelloContext(), resolveTrelloUrlFrom(github.context.payload.pull_request.body), github.context.payload.pull_request.html_url);
+                break;
+            case "move-card":
+                moveCard(createTrelloContext(), resolveTrelloUrlFrom(github.context.payload.pull_request.body), core.getInput("dest-list-id"));
+                break;
+            default:
+                throw new Error(`target action name cannot be resolved: ${targetActionName}`);
+        }
     }
 }
 catch (error) {
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,120 @@
 import * as core from "@actions/core";
 import * as github from "@actions/github";
+import fetch from "node-fetch";
+import log4js from "log4js";
+
+const logger = log4js.getLogger();
+
+interface TrelloContext {
+  apiKey: string;
+  apiToken: string;
+  boardId: string;
+}
+
+const resolveTrelloUrlFrom = (text?: string): Array<string> => {
+  logger.info(text);
+  if (text) {
+    return text.match(/https:\/\/trello\.com\/c\/.*/g) || [];
+  } else {
+    return [];
+  }
+};
+
+const createTrelloContext = () => {
+  const apiKey = process.env["TRELLO_API_KEY"];
+  const apiToken = process.env["TRELLO_API_TOKEN"];
+  const boardId = core.getInput("board-id");
+
+  if (!apiKey) {
+    throw new Error("TRELLO_API_KEY not defined");
+  }
+  if (!apiToken) {
+    throw new Error("TRELLO_API_TOKEN not defined");
+  }
+  if (!boardId) {
+    throw new Error("board-id not defined");
+  }
+  return {
+    apiKey,
+    apiToken,
+    boardId,
+  };
+};
+
+const attachPrToCard = (
+  trelloContext: TrelloContext,
+  trelloUrls: Array<string>,
+  prUrl?: string
+) => {
+  if (trelloUrls.length === 0) {
+    logger.info("target card not fount");
+    return;
+  }
+
+  logger.info(`target cards: ${trelloUrls}`);
+  if (!prUrl) {
+    throw new Error("pull-request url is not defined");
+  }
+
+  trelloUrls.forEach((url) => {
+    const cardId = (url.match(/https:\/\/trello\.com\/c\/(.*)/) ||
+      new Array(2))[1];
+    logger.info(`Attaching github url to card: ${cardId}`);
+    fetch(`https://trello.com/1/cards/${cardId}/attachments`, {
+      method: "POST",
+      body: new URLSearchParams({
+        key: trelloContext.apiKey,
+        token: trelloContext.apiToken,
+        url: prUrl,
+      }),
+    })
+      .then((r) => {
+        r.text().then((text) => logger.info(`response: ${text}`));
+        if (!r.ok) {
+          throw new Error(`error occurred while updating ${url}`);
+        }
+        logger.info(`successfully updated ${url}`);
+      })
+      .catch((error) => {
+        logger.info(`error: ${error}`);
+        throw error;
+      });
+  });
+};
+
+const moveCard = (
+  trelloContext: TrelloContext,
+  trelloUrls: Array<string>,
+  destListId: string
+) => {
+  logger.info("hoge");
+};
 
 try {
   const targetActionName = core.getInput("target-action-name");
-  console.log(targetActionName);
+  logger.info(`target action is ${targetActionName}`);
 
   if (github.context.payload.pull_request) {
-    const title = github.context.payload.pull_request.title;
-    const description = github.context.payload.pull_request.body;
-    const author = github.context.payload.pull_request.user.login;
-    console.log(title);
-    console.log(author);
-    console.log(description);
+    switch (targetActionName) {
+      case "attach-pr-to-card":
+        attachPrToCard(
+          createTrelloContext(),
+          resolveTrelloUrlFrom(github.context.payload.pull_request.body),
+          github.context.payload.pull_request.html_url
+        );
+        break;
+      case "move-card":
+        moveCard(
+          createTrelloContext(),
+          resolveTrelloUrlFrom(github.context.payload.pull_request.body),
+          core.getInput("dest-list-id")
+        );
+        break;
+      default:
+        throw new Error(
+          `target action name cannot be resolved: ${targetActionName}`
+        );
+    }
   }
 } catch (error) {
   core.setFailed(error.message);
